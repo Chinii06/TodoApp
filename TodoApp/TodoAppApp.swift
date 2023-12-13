41cfa68201acaaa86e12777837eb10c3b4653573
Joanne Wong <joannewong.wfg@gmail.com>
2023-12-12 22:15:39 -0500

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Joanne Wong on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
